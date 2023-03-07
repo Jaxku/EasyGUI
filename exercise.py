5e836bc183beb594f4e8e12bf743a239b6bcd614
Jaxku <52820208+Jaxku@users.noreply.github.com>
2023-03-08 10:15:40 +1300

    Initial commit

---
+++ exercise.py
@@ -0,0 +1,6 @@
+import easygui
+
+easygui.enterbox("Hi! What is your name? ")
+easygui.integerbox("How old are you?", "Age")
+easygui.buttonbox("Do you want to continue", "Game Continue", choices=["Yes", "No", "Maybe"])
+easygui.msgbox("Kia Ora! Welcome to Easygui")
