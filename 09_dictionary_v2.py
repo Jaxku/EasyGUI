0c66a8ba6f0f15959525444afad2bb79b63bfa44
Jaxku <52820208+Jaxku@users.noreply.github.com>
2023-03-20 10:05:12 +1300

    Create 09_dictionary_v2.py

---
+++ 09_dictionary_v2.py
@@ -0,0 +1,17 @@
+
+heros = {
+        "CoolMaster":
+        "name": "CoolMasterXx",
+        "power": "LaserSquirt",
+        "strength": 8
+
+
+        "ros":
+        "name": "ros",
+        "power": "Fibromaylga",
+        "strength": 10
+    }
+
+print("\nSuperhero 1 stats\n")
+for i in hero_0:
+    print(f"{i}: {hero_0[i]}")
